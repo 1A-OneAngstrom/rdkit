b24bfc864a2c35b79fc1aada8d59cf6ee0a9701e
gate <jocelyn.gate@oneangstrom.com>
2025-03-10 10:58:11 +0100

    small update

--- Code/cmake/RdkitSupport.cmake
+++ Code/cmake/RdkitSupport.cmake
@@ -1,17 +1,22 @@
 function(USE_RDKIT target)
-    if (NOT TARGET_HAS_RDKIT_${target})
-        set(TARGET_HAS_RDKIT_${target} TRUE CACHE INTERNAL "Track if ${target} already has RDKit")
+    # Locate rdkit_autogen.cpp.in in the RDKit installation directory
+    set(RDKIT_AUTOGEN_TEMPLATE "${RDKit_DIR}/Code/cmake/rdkit_autogen.cpp.in")
 
-        # Correctly locate rdkit_autogen.cpp.in in the same directory as this file (RdkitSupport.cmake)
-        set(RDKIT_AUTOGEN_TEMPLATE "${CMAKE_CURRENT_LIST_DIR}/rdkit_autogen.cpp.in")
+    # Destination file in the build directory
+    set(RDKIT_AUTOGEN_CPP "${CMAKE_CURRENT_BINARY_DIR}/rdkit_autogen.cpp")
 
-        # Destination file in the build directory
-        set(RDKIT_AUTOGEN_CPP "${CMAKE_CURRENT_BINARY_DIR}/rdkit_autogen.cpp")
+    # Copy the template file into the build directory
+    configure_file("${RDKIT_AUTOGEN_TEMPLATE}" "${RDKIT_AUTOGEN_CPP}" COPYONLY)
 
-        # Copy the template file into the build directory
-        configure_file("${RDKIT_AUTOGEN_TEMPLATE}" "${RDKIT_AUTOGEN_CPP}" COPYONLY)
+    # Print debug messages
+    message(STATUS "RDKit_DIR: ${RDKit_DIR}")
+    message(STATUS "Configuring RDKit autogen file: ${RDKIT_AUTOGEN_CPP} from ${RDKIT_AUTOGEN_TEMPLATE}")
 
-        # Inject the generated source file into the target
+    # Check if the file was successfully generated before adding it to the target
+    if (EXISTS "${RDKIT_AUTOGEN_CPP}")
+        message(STATUS "rdkit_autogen.cpp successfully generated, adding to target: ${target}")
         target_sources(${target} PRIVATE "${RDKIT_AUTOGEN_CPP}")
+    else()
+        message(WARNING "Failed to generate rdkit_autogen.cpp. RDKit features might not work in ${target}.")
     endif()
 endfunction()
